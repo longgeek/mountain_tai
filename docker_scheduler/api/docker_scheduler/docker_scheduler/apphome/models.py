9616bfcd5618455163bdc597e86a82e41a965d9b
frazy <frazy@thstack.com>
2013-11-29 06:20:07 -0800

    add models
    Change-Id: I505485cbff165878c87d298fcc493ccf4b280d77

--- docker_scheduler/api/docker_scheduler/docker_scheduler/apphome/models.py
+++ docker_scheduler/api/docker_scheduler/docker_scheduler/apphome/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from docker_scheduler.models import Image
+from docker_scheduler.models import Host
+from docker_scheduler.models import Container
 
 # Create your models here.
