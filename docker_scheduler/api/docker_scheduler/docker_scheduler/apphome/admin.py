a89b5f79b53ee10d2c92cfdf013dee0781980883
Longgeek <longgeek@thstack.com>
2014-11-21 17:34:59 +0800

    delete docker_scheduler rest api
    Change-Id: I560a1dfc4351c76603552bc9f7e046ce6acd5145

--- docker_scheduler/api/docker_scheduler/docker_scheduler/apphome/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from models import Image
-from models import Host
-from models import Container
-# Register your models here.
-
-admin.site.register(Image)
-admin.site.register(Host)
-admin.site.register(Container)
--- docker_scheduler/api/docker_scheduler/docker_scheduler/apphome/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from docker_scheduler.docker_scheduler.models import Image
-from docker_scheduler.docker_scheduler.models import Host
-from docker_scheduler.docker_scheduler.models import Container
-
-# Create your models here.
