d809ec0f49e9fed738a1e5081237c36a7aea4c52
longgeek <longgeek@thstack.com>
2014-12-23 02:32:01 +0800

    ffffffffffffuck database name docker
    Change-Id: Ic26862a537cdcce329a1569ff90dfc65214c0a10

--- mountain_tai/config.py
+++ mountain_tai/config.py
@@ -5,10 +5,10 @@ SECRET_KEY = '6vv0xbhmm(02ru1t*ztb-499^gnn_nrwc-is1^sv#j%ki*c9cw'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'docker',
+        'NAME': 'telegraph_pole',
         'HOST': '127.0.0.1',
         'USER': 'root',
-        'PASSWORD': '123123',
+        'PASSWORD': '',
     }
 }
 rediscon = redis.Redis(host="127.0.0.1", port=6379, db=0)
